e6bce518122a7aa88f0938e9d2d40a7e077592b8
paulccuno <pallccuno@gmail.com>
2025-04-30 16:16:43 -0500

    agregar auth-ms

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,56 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Inject,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
+import { catchError, firstValueFrom } from 'rxjs';
+import { NATS_SERVICE } from 'src/config';
+import { LoginUserDto, RegisterUserDto } from './dto';
+import { Request } from 'express';
+import { AuthGuard } from './guards/auth.guard';
+import { Token, User } from './decorators';
+import { CurrentUser } from './interfaces/current-user.interface';
+
+@Controller('auth')
+export class AuthController {
+  constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {}
+
+  @Post('register')
+  async registerUser(@Body() registerUserDto: RegisterUserDto) {
+    try {
+      const user = await firstValueFrom(
+        this.client.send('auth.register.user', registerUserDto),
+      );
+      return user;
+    } catch (error) {
+      throw new RpcException(error);
+    }
+  }
+
+  @Post('login')
+  async loginUser(@Body() loginUserDto: LoginUserDto) {
+    try {
+      const user = await firstValueFrom(
+        this.client.send('auth.login.user', loginUserDto),
+      );
+      return user;
+    } catch (error) {
+      throw new RpcException(error);
+    }
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('verify')
+  async verifyToken(@User() user: CurrentUser, @Token() token: string) {
+    try {
+      return { user, token };
+    } catch (error) {
+      throw new RpcException(error);
+    }
+  }
+}
